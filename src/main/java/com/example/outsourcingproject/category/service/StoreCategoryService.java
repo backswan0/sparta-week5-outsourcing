76a91bd86a35a0c484d0964b78740249a0730b9d
backswan0 <rnrwprh3434@gmail.com>
2025-01-22 00:36:17 +0900

    :art: style/jh-main-category (for store)
    - remove variables and classes

--- src/main/java/com/example/outsourcingproject/category/service/StoreCategoryService.java
+++ src/main/java/com/example/outsourcingproject/category/service/StoreCategoryService.java
@@ -1,8 +1,9 @@
 package com.example.outsourcingproject.category.service;
+
 import com.example.outsourcingproject.category.dto.request.CreateStoreCategoryRequestDto;
 import com.example.outsourcingproject.category.dto.response.CreateStoreCategoryResponseDto;
 import com.example.outsourcingproject.category.repository.CategoryRepository;
-import com.example.outsourcingproject.entity.Category;
+import com.example.outsourcingproject.entity.StoreCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,15 +12,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class StoreCategoryService {
+
     private final CategoryRepository categoryRepository;
+
     @Transactional
     public CreateStoreCategoryResponseDto createCategory(CreateStoreCategoryRequestDto requestDto) {
         // StoreCategory 엔티티 생성
-        Category storeCategoryToSave = new Category(requestDto.getName());
+        StoreCategory storeStoreCategoryToSave = new StoreCategory(requestDto.getName());
         // 엔티티 저장
-        Category savedStoreCategory = categoryRepository.save(storeCategoryToSave);
+        StoreCategory savedStoreStoreCategory = categoryRepository.save(
+            storeStoreCategoryToSave);
         // 응답 DTO 생성: savedStoreCategory에서 ID와 이름을 가져와 응답 DTO 생성
         // todo
-        return new CreateStoreCategoryResponseDto(savedStoreCategory.getId(), savedStoreCategory.getName());
+        return new CreateStoreCategoryResponseDto(
+            savedStoreStoreCategory.getId(),
+            savedStoreStoreCategory.getName()
+        );
     }
 }
--- src/main/java/com/example/outsourcingproject/store/repository/StoreCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.outsourcingproject.store.repository;
-
-import com.example.outsourcingproject.entity.StoreCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreCategoryRepository extends JpaRepository<StoreCategory, Long> {
-
-}
